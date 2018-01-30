b4e959060d9bed5cc584ea56b0a72ec8731af13b
NatashaLBallard <nbitcareer@gmail.com>
2018-01-30 10:56:51 -0500

    Initial commit

---
+++ src/main/java/com/infothroughurl/demo/MainController.java
@@ -0,0 +1,46 @@
+package com.infothroughurl.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Scanner;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String showIndex()
+    {
+        String userName="";
+        Scanner keyboard = new Scanner(System.in);
+        userName = keyboard.nextLine();
+        String greet = "Hello "+userName;
+        System.out.println(greet);
+
+        return greet;
+    }
+
+
+    @RequestMapping("/getname")
+    public String showWithName(@RequestParam("county") String name,@RequestParam("age") int age)
+    {
+
+        return name+" "+age;
+    }
+
+
+    @RequestMapping("/getothername")
+    public String showWithName(HttpServletRequest param)
+    {
+
+        String withName = param.getParameter("yourname");
+        if(withName==null)
+            return "Just Hello";
+        else return "Hello "+withName;
+    }
+
+
+
+}
